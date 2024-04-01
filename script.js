2690ff274b2cb388d52176f45c11a9d78e176c38
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-01 23:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", ()=>{
+
+
+   function displayImages(url){
+    let img = new Image()
+    return new Promise((resolve, reject)=>{
+        img.onload = (()=>{resolve(img)})
+        img.onerror = (()=>{reject(new error(`Failed to load image's URL: ${url}`))})
+    })
+   }
+
+   let p1 = displayImages(images[0].url)
+   let p2 = displayImages(images[1].url)
+   let p3 = displayImages(images[2].url)
+   let promiseArray = promise.all([p1,p2,p3])
+
+   promiseArray.then((img)=>{
+    for(let i=0; i<img.length; i++){
+        output.append(img)
+    }
+   })
+   .catch((err)=>{console.log(err)})
+})

c702ebe133b49a2ebbbb893fd48ef1802f3b06cf
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-01 23:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,17 +16,18 @@ btn.addEventListener("click", ()=>{
     return new Promise((resolve, reject)=>{
         img.onload = (()=>{resolve(img)})
         img.onerror = (()=>{reject(new error(`Failed to load image's URL: ${url}`))})
+        img.src = url;
     })
    }
 
    let p1 = displayImages(images[0].url)
    let p2 = displayImages(images[1].url)
    let p3 = displayImages(images[2].url)
-   let promiseArray = promise.all([p1,p2,p3])
+   let promiseArray = Promise.all([p1,p2,p3])
 
    promiseArray.then((img)=>{
     for(let i=0; i<img.length; i++){
-        output.append(img)
+        output.append(img[i])
     }
    })
    .catch((err)=>{console.log(err)})
